5efe730a69937a7196a49a51cae38156de9851ec
Einar W. Høst <einarwh@gmail.com>
2018-01-03 14:11:03 +0100

    Add basic project.

---
+++ Halite/Halite/HalLink.cs
@@ -0,0 +1,32 @@
+using System;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+
+namespace Halite
+{
+    /// <summary>
+    /// Represents a HAL link whose Href value is a URI [RFC3986]
+    /// </summary>
+    [Serializable]
+    public class HalLink : HalLinkObject
+    {
+        [JsonConstructor]
+        public HalLink([CanBeNull] string href)
+            : base(href)
+        {
+        }
+
+        public HalLink([CanBeNull] Uri href) : base(href)
+        {
+        }
+
+        /// <summary>
+        /// This is not a templated link (you would use HalTemplatedLink for that). 
+        /// Why do we return null instead of false? According to the HAL specification, 
+        /// the value of the templated property SHOULD be considered false if it is undefined. 
+        /// By choosing undefined over false, we avoid including the templated property 
+        /// for non-templated links during serialization.
+        /// </summary>
+        public override bool? Templated => null;
+    }
+}
---
+++ Halite/Halite/SelfLink.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+
+namespace Halite
+{
+    public sealed class SelfLink : HalLink
+    {
+        public SelfLink([CanBeNull] string href) : base(href)
+        {
+        }
+    }
+}
