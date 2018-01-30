f37a28e55d41ac1612d12bc005a5836f73c2bb54
Einar W. Høst <einarwh@gmail.com>
2018-01-30 18:04:59 +0100

    Add code analyzer and fix.

---
+++ source/analyzer/Halite.Analyzer/HalLinkSubclassAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Halite
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class HalLinkSubclassAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticIdProperty = "HalLinkPropertyDeclaration";
+
+        private const string Category = "Design";
+
+        private static readonly DiagnosticDescriptor HalLinkPropertyRule =
+            new DiagnosticDescriptor(
+                DiagnosticIdProperty,
+                "HalLink subclasses should not define any additional properties.",
+                "{0} should only use properties of the superclass and not define any additional ones.",
+                Category,
+                DiagnosticSeverity.Warning,
+                isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(HalLinkPropertyRule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzePropertyDeclarationSyntaxNode, SyntaxKind.PropertyDeclaration);
+        }
+
+        /// <summary>
+        /// Types of properties in a HalLinks subclass must be (subtypes of) HalLink.
+        /// </summary>
+        /// <param name="context"></param>
+        private static void AnalyzePropertyDeclarationSyntaxNode(SyntaxNodeAnalysisContext context)
+        {
+            var declarationNode = (PropertyDeclarationSyntax)context.Node;
+            var propertySymbol = context.SemanticModel.GetDeclaredSymbol(declarationNode);
+            var containingType = propertySymbol.ContainingType;
+            if (containingType.InheritsFromHalLink() || containingType.InheritsFromHalTemplatedLink())
+            {
+                var diagnostic = Diagnostic.Create(HalLinkPropertyRule, declarationNode.GetLocation(),
+                    propertySymbol.ContainingType.Name);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
---
+++ source/analyzer/Halite.Analyzer/PropertyHalLinkIEnumerableCodeFixProvider.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Halite
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(PropertyHalLinkIEnumerableCodeFixProvider)), Shared]
+    public class PropertyHalLinkIEnumerableCodeFixProvider : CodeFixProvider
+    {
+        private const string Title = "Change return type to IEnumerable<HalLink>.";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(HalLinksSubclassAnalyzer.DiagnosticIdProperty);
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var syntaxNode = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf();
+            var declaration = syntaxNode.OfType<PropertyDeclarationSyntax>().First();
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: Title,
+                    createChangedDocument: c => FixPropertyReturnType(context.Document, declaration, c),
+                    equivalenceKey: Title),
+                diagnostic);
+        }
+
+        private static async Task<Document> FixPropertyReturnType(Document document, PropertyDeclarationSyntax propertyDeclarationSyntax, CancellationToken cancellationToken)
+        {
+            TypeSyntax halLinkTypeSyntax = SyntaxFactory.IdentifierName(SyntaxFactory.Identifier("HalLink"));
+            var ienumerableTypeSyntax = 
+                SyntaxFactory.GenericName(SyntaxFactory.Identifier("IEnumerable"),
+                SyntaxFactory.TypeArgumentList(SyntaxFactory.SeparatedList(new[] { halLinkTypeSyntax })));
+
+            var replacementSyntax = propertyDeclarationSyntax.WithType(ienumerableTypeSyntax);
+            var oldRoot = await document.GetSyntaxRootAsync(cancellationToken);
+            var newRoot = oldRoot.ReplaceNode(propertyDeclarationSyntax, replacementSyntax);
+            return document.WithSyntaxRoot(newRoot);
+        }
+
+        public static TypeSyntax GetTypeSyntax(string identifier, TypeSyntax argument)
+        {
+            return
+                SyntaxFactory.GenericName(SyntaxFactory.Identifier(identifier),
+                SyntaxFactory.TypeArgumentList(SyntaxFactory.SeparatedList(new [] { argument })));
+        }
+
+    }
+}
