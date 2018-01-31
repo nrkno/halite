41c0a12ad57eec8f538a7e58d6d97a11436d78b9
Harald Schult Ulriksen <harald@ulriksen.net>
2018-01-31 13:18:48 +0100

    Tests for private property analyzer and fix

---
+++ source/analyzer/Halite.Analyzer.Test/HalLinksPropertySetterTests.cs
@@ -0,0 +1,265 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Reflection;
+using TestHelper;
+
+
+namespace Halite.Analyzer.Test
+{
+    [TestClass]
+    public class PropertySetterTests : CodeFixVerifier
+    {
+        [TestMethod]
+        public void Property_DiagnosticsNotAppliedOnNormalClass()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public class TypeName 
+        {   
+            public HalLink SomeLink { get; set; }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test, new DiagnosticResult[0]);
+        }
+
+        [TestMethod]
+        public void Property_PublicSetterNotAllowed()
+        {
+            var test = @"
+    using Halite;
+    namespace ConsoleApplication1
+    {
+        public class TypeName : HalLinks
+        {   
+            public static void Main(string[] args)
+            {
+            }
+
+            public TypeName(SelfLink self) : base(self)
+            { }
+
+            public Halite.HalLink SomeLink { get; set; }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "HalLinksPropertyNoPublicSetter",
+                Message = String.Format("Property SomeLink in TypeName, a subclass of HalLinks, must not have externally accessible setter."),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 14, 51)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public void Property_PrivateSetterAllowed()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName : Halite.HalLinks
+        {   
+            public HalLink SomeLink { get; private set; }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "HalLinksPropertyDeclaration",
+                Message = String.Format("Property SomeLink in TypeName, a subclass of HalLinks, must not have externally accessible setter."),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 13, 44)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, new DiagnosticResult[0]);
+        }
+
+        [TestMethod]
+        public void Property_InternalSetterNotAllowed()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName : Halite.HalLinks
+        {   
+            public HalLink SomeLink { get; internal set; }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "HalLinksPropertyNoPublicSetter",
+                Message = String.Format("Property SomeLink in TypeName, a subclass of HalLinks, must not have externally accessible setter."),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 13, 44)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public void Property_ProtectedSetterNotAllowed()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName : Halite.HalLinks
+        {   
+            public HalLink SomeLink { get; protected set; }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "HalLinksPropertyNoPublicSetter",
+                Message = String.Format("Property SomeLink in TypeName, a subclass of HalLinks, must not have externally accessible setter."),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 13, 44)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public void Property_PrivateSetterFix_Applied()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public class TypeName : Halite.HalLinks
+        {   
+            public Halite.HalLink SomeLink { get; set; }
+        }
+    }";
+
+            var fixedTest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public class TypeName : Halite.HalLinks
+        {   
+            public Halite.HalLink SomeLink { get; private set; }
+        }
+    }";
+            VerifyCSharpFix(test, fixedTest);
+        }
+
+        [TestMethod]
+        public void Property_InternalSetterFix_Applied()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public class TypeName : Halite.HalLinks
+        {   
+            public Halite.HalLink SomeLink { get; internal set; }
+        }
+    }";
+
+            var fixedTest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public class TypeName : Halite.HalLinks
+        {   
+            public Halite.HalLink SomeLink { get; private set; }
+        }
+    }";
+            VerifyCSharpFix(test, fixedTest);
+        }
+
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider() => new HalLinksChangePropertySetterToPrivateCodeFixProvider();
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new HalLinksPropertySetterAnalyzer();
+
+        protected override MetadataReference[] GetReferences()
+        {
+            var path = AssemblyDirectory;
+
+            return new[] {
+                MetadataReference.CreateFromFile(Path.Combine(path,"Halite.dll")) };
+        }
+
+        private static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+    }
+}
