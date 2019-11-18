d074b3f049b103fc34af2755c890690d91437a2b
Steinar Aasheim <steinar.aasheim@nrk.no>
2019-11-18 18:33:56 +0100

    Revert "To netstandard2.0 (#13)"
    This reverts commit ff3c488cf86955c912fc681ec28e2266e1c3fa75.

---
+++ source/analyzer/Halite.Analyzer/HalLinksChangePropertySetterToPrivateCodeFixProvider.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Halite
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(HalLinksChangePropertySetterToPrivateCodeFixProvider)), Shared]
+    public class HalLinksChangePropertySetterToPrivateCodeFixProvider : CodeFixProvider
+    {
+        private const string Title = "Mark setter as private";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(HalLinksSubclassPropertySetterAnalyzer.DiagnosticIdProperty);
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var syntaxNode = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf();
+            var declaration = syntaxNode.OfType<PropertyDeclarationSyntax>().First();
+
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: Title,
+                    createChangedDocument: c => MarkPropertySetterPrivate(context.Document, declaration, c),
+                    equivalenceKey: Title),
+                diagnostic);
+        }
+
+        private static async Task<Document> MarkPropertySetterPrivate(Document document, PropertyDeclarationSyntax propertyDeclarationSyntax, CancellationToken cancellationToken)
+        {
+            var oldSetter = propertyDeclarationSyntax.AccessorList.Accessors.Single(a => a.Keyword.Kind() == SyntaxKind.SetKeyword);
+
+            var newSetter = oldSetter
+                .WithModifiers(SyntaxTokenList.Create(SyntaxFactory.Token(SyntaxKind.PrivateKeyword)));
+
+            var oldRoot = await document.GetSyntaxRootAsync(cancellationToken);
+            var newRoot = oldRoot.ReplaceNode(oldSetter, newSetter);
+            return document.WithSyntaxRoot(newRoot);
+        }
+    }
+}
